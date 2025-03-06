7307be805110acc0282a39681130eecabe835c50
tenyon61 <tenyon@cqbo.com>
2025-03-07 00:10:00 +0800

    fix: 英文页码问题，滚动条问题

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <a-config-provider :theme="appStore.theme">
+  <a-config-provider :theme="appStore.theme" :locale="locale">
     <router-view />
   </a-config-provider>
 </template>
 
 <script lang="ts" setup>
 import { useAppStore } from '@/store/appStore.ts'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import dayjs from 'dayjs'
+import 'dayjs/locale/zh-cn'
 
+dayjs.locale('zh-cn')
 const appStore = useAppStore()
+const locale = zhCN
 </script>
 
 <style scoped lang="scss"></style>
